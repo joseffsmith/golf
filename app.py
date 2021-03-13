a1ee985c609b19661f2c365bb35dd72f2c249bf9
Joe Smith <joseffsmith@icloud.com>
2021-03-13 18:56:35 +0000

    update app for live

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
+from dotenv import load_dotenv
 
 from scraper import MasterScoreboard
 from ms_parser import Parser
@@ -8,10 +9,9 @@ from asset import Library
 app = Flask(__name__)
 CORS(app)
 
-from dotenv import load_dotenv
 load_dotenv()
 API_SECRET = os.getenv('API_SECRET')
-LIVE=os.getenv('LIVE')
+LIVE = os.getenv('LIVE')
 
 
 @app.before_request
@@ -20,11 +20,13 @@ def before_request():
     if key != API_SECRET:
         abort(401)
 
+
 @app.route('/comps/', methods=['GET'])
 def index():
     lib = Library(live=LIVE)
     comps = lib.read('curr_comps')
     return jsonify(status='ok', comps=comps)
+
 
 @app.route('/scrape/', methods=['POST'])
 def scrape():
@@ -37,10 +39,12 @@ def scrape():
 
     parsed = Parser(content=content).parse()
 
-    lib=Library(live=LIVE)
+    lib = Library(live=LIVE)
     lib.write('curr_comps', parsed)
-    
+
     return jsonify(status='ok', comps=parsed)
 
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(port=5000)
