e1cdfdd79cd3672570582ca2c592249aee87e82a
Joe Smith <joseffsmith@icloud.com>
2021-03-24 17:40:21 +0000

    update times

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const puppeteer = require('puppeteer');
     }
 
     // date of competition in future
-    const desired_date = '27 Mar'
+    const desired_date = '31 Mar'
 
     // unique word in the competition title to distinguish it from other comps on the same day
     const keyword = undefined
@@ -38,8 +38,7 @@ const puppeteer = require('puppeteer');
         // '10:00', '10:10', '10:20', '10:30', '10:40', '10:50',
         // '12:00', '12:10', '12:20', '12:30', '12:40', '12:50',
         // '13:00', '13:10', '13:20', '13:30', '13:40', '13:50',
-        '14:20', '14:30', '14:40',
-        '15:00', '15:10', '15:20', '15:30', '15:40', '15:50',
+        '15:00', '15:20', '15:40', '15:50',
     ]
 
     // ID's for the <select> of the <option> values of the players we want to play with
