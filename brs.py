9360d0b223d4258f00e6bfe2914206c99503e1f5
Joe Smith <26124821+joseffsmith@users.noreply.github.com>
2022-10-29 15:38:14 +0100

    Create brs.py

---
+++ brs.py
@@ -0,0 +1,38 @@
+import requests
+import bs4
+from pprint import pprint
+
+USERNAME=71451
+PASSWORD="arsenal"
+
+
+def login():
+    session = requests.Session()
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0'
+    session.headers = {'User-Agent': user_agent}
+    resp = session.get('https://members.brsgolf.com/thevalehotelspa/login')
+
+    b = bs4.BeautifulSoup(resp.content, features='html.parser')
+
+    token = b.find(attrs={'id':'login_form__token'}).get('value')
+
+    print('token: ', token)
+    
+    resp = session.post('https://members.brsgolf.com/thevalehotelspa/login', data={
+        'login_form[username]': USERNAME,
+        'login_form[password]': PASSWORD,
+        'login_form[login]': '',
+        'login_form[_token]': token
+    })
+
+    
+    resp = session.get('https://members.brsgolf.com/thevalehotelspa/tee-sheet/data/1/2022/11/04')
+    print(resp)
+    print(resp.headers)
+    pprint(resp.json())
+
+
+
+
+if __name__ == '__main__':
+    login()
