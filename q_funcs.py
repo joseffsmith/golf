f66cb801256c1bfeec31bbbaf50835e19dbe4be8
Joe Smith <joseffsmith@icloud.com>
2025-02-02 10:29:11 +0000

    fix

--- q_funcs.py
+++ q_funcs.py
@@ -19,7 +19,7 @@ PASSWORD = os.getenv('INT_PASSWORD')
 from better_app import tryBookSquash
 
 def main():
-    if not REDIS_HOST or not REDIS_PASS:
+    if not REDIS_HOST:
         raise Exception('REDIS_HOST and REDIS_PASS must be set in .env')
     
     connection = Redis(host=REDIS_HOST, port=6379, password=REDIS_PASS)
